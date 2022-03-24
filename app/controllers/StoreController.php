992bc8face3cbe638c038df0e6a45cbf67c0b019
Taino Corentin <cprentintaino2b@gmail.com>
2022-03-24 16:21:03 +0100

    Correction de l affichage des images + tentative d ajout du petit icone shop

--- app/controllers/StoreController.php
+++ app/controllers/StoreController.php
@@ -13,10 +13,13 @@ use Ubiquity\orm\DAO;
  */
 class StoreController extends \controllers\ControllerBase
 {
+    private $a = []; //Passer $a en variable de session
+
     public function index()
     {
+        $this->a['count'] = 0;
         $section=DAO::getAll(Section::class);
-        $this->loadView('@activeTheme/main/vMenu.html');
+        $this->loadView('@activeTheme/main/vMenu.html', ['count'=>$this->a['count']]);
         $this->loadView("StoreController/index.html",['section'=>$section]);
     }
 
@@ -24,25 +27,29 @@ class StoreController extends \controllers\ControllerBase
     #[Route(path: "section/{idSection}", name: "store.section")]
     public function storeSection($idSection)
     {
+        if(!isset($this->a['count']))
+            $this->a['count'] = 0;
         $section=DAO::getbyId(Section::class, $idSection,['products']);
-        $this->loadView('@activeTheme/main/vMenu.html');
+        $this->loadView('@activeTheme/main/vMenu.html', ['count'=>$this->a['count']]);
         $this->loadView("StoreController/storeSection.html", ['section'=>$section]);
     }
 
     #[Route(path: "allProducts", name: "store.allProducts")]
     public function allProduct()
     {
+        if(!isset($this->a['count']))
+            $this->a['count'] = 0;
         $products=DAO::getAll(Product::class);
-        $this->loadView('@activeTheme/main/vMenu.html');
+        $this->loadView('@activeTheme/main/vMenu.html',['count'=>$this->a['count']]);
         $this->loadView("StoreController/storeSectionBis.html", ['products'=>$products]);
     }
 
     #[Get(path: "section/{idProduct}/{count}", name: "store.addToCart")]
     public function addToCart(int $idProduct,int $count)
     {
-        //$_SESSION["count"]= $_SESSION["count"] + $count;
-
-        $this->loadView('@activeTheme/main/vMenu.html');
+        $this->a['count'] = $this->a['count'] + 1;
+        $products=DAO::getAll(Product::class);
+        $this->loadView('@activeTheme/main/vMenu.html', ['count'=>$this->a['count']]);
         $this->loadView("StoreController/storeSectionBis.html", ['products'=>$products]);
     }
 
