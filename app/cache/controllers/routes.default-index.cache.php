1f3b9442446ec632db72c8af418101da3ee8d872
Taino Corentin <cprentintaino2b@gmail.com>
2022-03-24 14:06:23 +0100

    Creation du controller StoreController ajout de son index en route par default dans le fichier service.php

---
+++ app/cache/controllers/routes.default-index.cache.php
@@ -0,0 +1,2 @@
+<?php
+return array("delete"=>["/delete/(.+?)/"],"edit"=>["/edit/(.+?)/"],"loadList"=>["/loadList/(.+?)/"],"new"=>["/new/(.+?)/"]);
---
+++ app/controllers/StoreController.php
@@ -0,0 +1,13 @@
+<?php
+namespace controllers;
+ use Ubiquity\attributes\items\router\Route;
+
+ /**
+  * Controller StoreController
+  */
+class StoreController extends \controllers\ControllerBase{
+
+	public function index(){
+		
+	}
+}
