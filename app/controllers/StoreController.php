143bdafe29fbf9bdb058a738a9c963c60a7ac319
Taino Corentin <cprentintaino2b@gmail.com>
2022-03-24 15:57:56 +0100

    les liens entre les différentes pages sont maintenants fonctionnel

--- app/controllers/StoreController.php
+++ app/controllers/StoreController.php
@@ -3,6 +3,7 @@ namespace controllers;
 use models\Product;
 use models\Section;
 use Ubiquity\attributes\items\di\Autowired;
+use Ubiquity\attributes\items\router\Get;
 use Ubiquity\attributes\items\router\Route;
 use Ubiquity\controllers\auth\WithAuthTrait;
 use Ubiquity\orm\DAO;
@@ -21,7 +22,7 @@ class StoreController extends \controllers\ControllerBase
 
 
     #[Route(path: "section/{idSection}", name: "store.section")]
-    public function storeSection(int $idSection)
+    public function storeSection($idSection)
     {
         $section=DAO::getbyId(Section::class, $idSection,['products']);
         $this->loadView('@activeTheme/main/vMenu.html');
@@ -32,6 +33,15 @@ class StoreController extends \controllers\ControllerBase
     public function allProduct()
     {
         $products=DAO::getAll(Product::class);
+        $this->loadView('@activeTheme/main/vMenu.html');
+        $this->loadView("StoreController/storeSectionBis.html", ['products'=>$products]);
+    }
+
+    #[Get(path: "section/{idProduct}/{count}", name: "store.addToCart")]
+    public function addToCart(int $idProduct,int $count)
+    {
+        //$_SESSION["count"]= $_SESSION["count"] + $count;
+
         $this->loadView('@activeTheme/main/vMenu.html');
         $this->loadView("StoreController/storeSectionBis.html", ['products'=>$products]);
     }
